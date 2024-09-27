84cafa7d3d5a38091bed81c7f1c9783982e56701
Lee <48121658+lee2nd@users.noreply.github.com>
2024-09-27 14:41:52 +0800

    Create docker-compose.sh

---
+++ docker-compose.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 定義需要停止和刪除的容器名稱
+containers=("l4a_etl" "l6b_fs_eb16c_etl" "l6b_fs_eb17c_etl" "l6b_sw_at2_etl" "l6b_sw_tc01_etl" "l6b_sw_tc02_etl")
+
+# 停止並刪除現有容器，如果存在
+for container in "${containers[@]}"; do
+    if [ "$(docker ps -aq -f name=$container)" ]; then
+        echo "Stopping existing container: $container..."
+        docker stop $container
+        echo "Removing existing container: $container..."
+        docker rm $container
+    fi
+done
+
+# 使用 Docker Compose 啟動新的容器
+USER_ID=$(id -u) GROUP_ID=$(id -g) docker-compose up -d
